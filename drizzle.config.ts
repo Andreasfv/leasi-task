0389fee3bdc291fa59fad90ddc00cfd2ab6f64d1
Andreas Frenning Vangen <andreas.vangen@gmail.com>
2024-01-24 08:38:32 +0100

    project init

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env.js";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "better-sqlite",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  out: "./src/server/db/migrations",
+  tablesFilter: ["leasi-task_*"],
+} satisfies Config;
